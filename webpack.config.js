b121db3b59e6503e14226304ccc94539b1701150
AshakaE <ashakaegerega@gmail.com>
2021-04-13 20:51:30 +0100

    Add dotenv config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, './src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv(
+      path.resolve(__dirname, '.env'),
+    ),
+  ],
+};
