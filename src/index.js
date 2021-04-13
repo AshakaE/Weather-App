d5c40c1fe15ad9854a3a7d85bf00631255c63054
AshakaE <ashakaegerega@gmail.com>
2021-04-13 21:45:02 +0100

    Add html to display data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 /* eslint-disable no-console */
 const envVars = process.env.OPENWEATHER_KEY;
+
+const visibility = document.querySelector('[data-vis]');
+const wind = document.querySelector('[data-wind]');
+const humidity = document.querySelector('[data-hum]');
+const desc = document.querySelector('[data-desc]');
+const temp = document.querySelector('[data-temp]');
+const city = document.querySelector('[data-city]');
 const input = document.querySelector('[data-input]');
 const button = document.querySelector('[data-submit]');
 
@@ -13,11 +20,12 @@ button.addEventListener('click', () => {
       const visValue = (data.visibility) / 1000;
       const humValue = data.main.humidity;
       const windValue = (data.wind.speed).toFixed(1);
-      const all = {
-        tempValue, cityValue, descValue, visValue, humValue, windValue,
-      };
-      // eslint-disable-next-line no-console
-      console.log(data);
-      console.log(all);
+
+      city.innerHTML = cityValue;
+      desc.innerHTML = descValue;
+      temp.innerHTML = `${+tempValue}&deg;C`;
+      wind.innerHTML = `${windValue}m/s`;
+      humidity.innerHTML = `${humValue}%`;
+      visibility.innerHTML = `${visValue}km`;
     });
 });
