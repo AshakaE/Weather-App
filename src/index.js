96bb1c55d474cc0e2ec9e8a72867c03e062884e2
AshakaE <ashakaegerega@gmail.com>
2021-04-14 14:01:32 +0100

    Add display icons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import * as display from './display';
+
 /* eslint-disable no-console */
 const envVars = process.env.OPENWEATHER_KEY;
 
@@ -14,12 +16,33 @@ button.addEventListener('click', () => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=${envVars}`)
     .then(response => response.json())
     .then(data => {
+      input.value = null;
       const tempValue = data.main.temp;
       const cityValue = data.name;
       const descValue = data.weather[0].description;
       const visValue = (data.visibility) / 1000;
       const humValue = data.main.humidity;
       const windValue = (data.wind.speed).toFixed(1);
+      console.log(data);
+      console.log(descValue);
+
+      if (descValue.includes('snow')) {
+        display.renderSnow();
+      } else if (descValue.includes('clear')) {
+        display.renderSun();
+      } else if (descValue.includes('scattered')) {
+        display.renderCloud();
+      } else if (descValue.includes('moderate')) {
+        display.renderRain();
+      } else if (descValue.includes('overcast')) {
+        display.renderCloudy();
+      } else if (descValue.includes('heavy')) {
+        display.renderStorm();
+      } else if (descValue.includes('light')) {
+        display.renderSunnyRain();
+      } else if (descValue.includes('few')) {
+        display.renderSunnyCloud();
+      }
 
       city.innerHTML = cityValue;
       desc.innerHTML = descValue;
